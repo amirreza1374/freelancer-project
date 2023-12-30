94f70a6ba777b8153f6013dcfd67f41bbeee8916
amirreza1374 <126147703+amirreza1374@users.noreply.github.com>
2023-12-30 09:12:43 +0330

    add:loading and some other logic

--- src/features/authentication/CheckOTPForm.jsx
+++ src/features/authentication/CheckOTPForm.jsx
@@ -1,12 +1,58 @@
-import { useState } from "react";
+import { useMutation } from "@tanstack/react-query";
+import { useEffect, useState } from "react";
 import OTPInput from "react-otp-input";
+import { checkOtp } from "../../services/authService";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+import { HiArrowRight } from "react-icons/hi";
+import { CiEdit } from "react-icons/ci";
 
-function CheckOTPForm() {
+const RESEND_TIME = 90;
+
+function CheckOTPForm({ phoneNumber, onBack, onResendOtp, otpResponse }) {
   const [otp, setOtp] = useState("");
+  const navigate = useNavigate();
+  const [time, setTime] = useState(RESEND_TIME);
+  const { isPending, mutateAsync } = useMutation({
+    mutationFn: checkOtp,
+  });
+
+  const checkOtpHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const { user, message } = await mutateAsync({ phoneNumber, otp });
+      toast.success(message);
+      if (user.active) {
+        // if (user.role === "OWNER") navigate("/owner");
+        // if (user.role === "FREELANCER") navigate("/freelancer");
+      } else {
+        navigate("/complete-profile");
+      }
+    } catch (error) {}
+    toast.error(error?.response?.data?.message);
+  };
+
+  useEffect(() => {
+    const timer = time > 0 && setInterval(() => setTime((t) => t - 1), 1000);
+    return () => {
+      if (timer) clearInterval(timer);
+    };
+  }, [time]);
 
   return (
     <div>
-      <form className="space-y-10">
+      <button onClick={onBack}>
+        <HiArrowRight className="size-6 text-secondary-500" />
+      </button>
+      {otpResponse && (
+        <p className="flex items-center gap-x-2 my-4">
+          {otpResponse?.message}
+          <button onClick={onBack}>
+            <CiEdit className="size-6 text-secondary-900" />
+          </button>
+        </p>
+      )}
+      <form onSubmit={checkOtpHandler} className="space-y-10">
         <p className="font-bold text-secondary-800">کد تایید را وارد کنید</p>
         <OTPInput
           value={otp}
@@ -22,7 +68,22 @@ function CheckOTPForm() {
             borderRadius: "0.5rem",
           }}
         />
-        <button className="btn btn--primary w-full">تایید</button>
+        <div className="">
+          {isPending ? (
+            <Loading />
+          ) : (
+            <button type="submit" className="btn btn--primary w-full">
+              تایید
+            </button>
+          )}
+        </div>
+        <div className="text-secondary-500">
+          {time > 0 ? (
+            <p>{time} تا ارسال مجدد کد</p>
+          ) : (
+            <button onClick={onResendOtp}>ارسال مجدد کد تایید</button>
+          )}
+        </div>
       </form>
     </div>
   );
