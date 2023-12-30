309c6a79a2ffb84d9a37a5bf4402dd4a90264d38
amirreza1374 <126147703+amirreza1374@users.noreply.github.com>
2023-12-30 09:08:17 +0330

    add: radioInput component

---
+++ src/ui/RadioInput.jsx
@@ -0,0 +1,20 @@
+function RadioInput({ label, value, onChange, name, id, checked }) {
+  return (
+    <div>
+      <div className="flex items-center gap-x-2 text-secondary-600">
+        <input
+          className="cursor-pointer size-4"
+          type="radio"
+          name={name}
+          id={id}
+          value={value}
+          onChange={onChange}
+          checked={checked}
+        />
+        <label htmlFor="OWNER">{label}</label>
+      </div>
+    </div>
+  );
+}
+
+export default RadioInput;
