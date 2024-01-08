714e0adbf97802466c1d25207c933a92a6651b91
amirreza1374 <126147703+amirreza1374@users.noreply.github.com>
2024-01-08 11:27:44 +0330

    add: dark mode styles

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -26,13 +26,13 @@ export default Sidebar;
 
 function CustomNavlink({ children, to }) {
   const navlinkClasses =
-    "flex items-center gap-x-2 hover:bg-primary-100/50 hover:text-primary-900 px-2 py-1.5 rounded-lg transition-all duration-300";
+    "flex items-center gap-x-2 hover:bg-primary-100/30 hover:text-primary-900 px-2 py-1.5 rounded-lg transition-all duration-300";
   return (
     <NavLink
       to={to}
       className={({ isActive }) =>
         isActive
-          ? `${navlinkClasses} bg-primary-100/50 text-primary-900`
+          ? `${navlinkClasses} bg-primary-100/80 text-primary-900`
           : `${navlinkClasses} text-secondary-600`
       }
     >
