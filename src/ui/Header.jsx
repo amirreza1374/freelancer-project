353a5bc9a66505521b763070619e384f89162182
amirreza1374 <126147703+amirreza1374@users.noreply.github.com>
2024-01-08 09:56:13 +0330

    add: user avatar and header menu component

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,10 +1,21 @@
-import React from "react";
+import UserAvatar from "../features/authentication/UserAvatar";
 import useUser from "../features/authentication/useUser";
+import HeaderMenu from "./HeaderMenu";
 
 function Header() {
-  const { data } = useUser();
-
-  return <div className="bg-secondary-0 py-4 px-8">app header</div>;
+  const { isLoading } = useUser();
+  return (
+    <div className="bg-secondary-0 py-4 px-8 border-b border-secondary-200">
+      <div
+        className={`container xl:max-w-screen-lg flex gap-x-8 items-center justify-end ${
+          isLoading ? "blur-sm opacity-50" : ""
+        }`}
+      >
+        <UserAvatar />
+        <HeaderMenu />
+      </div>
+    </div>
+  );
 }
 
 export default Header;
