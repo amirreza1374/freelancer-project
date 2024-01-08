c24a2eacbb609529ac9b67b87fa7714a97c69963
amirreza1374 <126147703+amirreza1374@users.noreply.github.com>
2024-01-08 11:40:48 +0330

    add: owner layout

---
+++ src/features/owner/OwnerLayout.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import AppLayout from "../../ui/AppLayout";
+import Sidebar from "../../ui/Sidebar";
+import CustomNavlink from "../../ui/CustomNavlink";
+import { HiCollection, HiHome } from "react-icons/hi";
+
+function OwnerLayout() {
+  return (
+    <AppLayout>
+      <Sidebar>
+        <CustomNavlink to="/owner/dashboard">
+          <HiHome />
+          <span>داشبورد</span>
+        </CustomNavlink>
+
+        <CustomNavlink to="/owner/projects">
+          <HiCollection />
+          <span>پروژه ها</span>
+        </CustomNavlink>
+      </Sidebar>
+    </AppLayout>
+  );
+}
+
+export default OwnerLayout;
