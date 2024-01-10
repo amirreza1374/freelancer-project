358e15c6bc4854527dbf3401d385b519b511a4b5
amirreza1374 <126147703+amirreza1374@users.noreply.github.com>
2024-01-10 11:31:12 +0330

    add: dashboard layout

---
+++ src/features/freelancer/DashboardLayout.jsx
@@ -0,0 +1,18 @@
+import Stats from "./Stats";
+import Loading from "../../ui/Loading";
+import useProposals from "../proposals/useProposals";
+import DashboardHeader from "../../ui/DashboardHeader";
+
+function DashboardLayout() {
+  const { isLoading, proposals } = useProposals();
+
+  if (isLoading) return <Loading />;
+  return (
+    <div>
+      <DashboardHeader />
+      <Stats proposals={proposals} />
+    </div>
+  );
+}
+
+export default DashboardLayout;
