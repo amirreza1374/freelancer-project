9e9dd307de421817df623f9d7566ebcd33d08f64
amirreza1374 <126147703+amirreza1374@users.noreply.github.com>
2024-01-10 11:36:25 +0330

    add: submitedProject component

---
+++ src/pages/SubmittedProjects.jsx
@@ -0,0 +1,14 @@
+import ProjectsHeader from "../features/freelancer/project/ProjectsHeader";
+
+import ProjectsTable from "../features/freelancer/project/ProjectsTable";
+
+function SubmittedProjects() {
+  return (
+    <div>
+      <ProjectsHeader />
+      <ProjectsTable />
+    </div>
+  );
+}
+
+export default SubmittedProjects;
