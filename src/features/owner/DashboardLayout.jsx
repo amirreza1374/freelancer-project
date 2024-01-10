2d83427eaa7870532cc3827e5eb178044d6ffc8c
amirreza1374 <126147703+amirreza1374@users.noreply.github.com>
2024-01-10 11:33:19 +0330

    edit: dashboard header import link

--- src/features/owner/DashboardLayout.jsx
+++ src/features/owner/DashboardLayout.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import DashboardHeader from "./DashboardHeader";
+import DashboardHeader from "../../ui/DashboardHeader";
 import Stats from "./Stats";
 import useOwnerProjects from "../projects/useOwnerProjects";
 import Loading from "../../ui/Loading";
