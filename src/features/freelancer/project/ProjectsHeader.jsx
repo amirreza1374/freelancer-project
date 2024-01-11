ca37ae74e2bbea169d75df3b3c1971961a470444
amirreza1374 <126147703+amirreza1374@users.noreply.github.com>
2024-01-11 12:27:50 +0330

    add: filters to projectsHeader

--- src/features/freelancer/project/ProjectsHeader.jsx
+++ src/features/freelancer/project/ProjectsHeader.jsx
@@ -1,10 +1,50 @@
 import React from "react";
+import FilterDropDown from "../../../ui/FilterDropDown";
+import useCategories from "../../../hooks/useCategories";
+import Filter from "../../../ui/Filter";
+
+const sortOptions = [
+  {
+    label: "مرتب سازی(جدیدترین)",
+    value: "latest",
+  },
+  {
+    label: "مرتب سازی(قدیمی‌ترین)",
+    value: "earliest",
+  },
+];
+
+const statusOptions = [
+  {
+    label: "همه",
+    value: "ALL",
+  },
+  {
+    label: "باز",
+    value: "OPEN",
+  },
+  {
+    label: "بسته",
+    value: "CLOSED",
+  },
+];
 
 function ProjectsHeader() {
+  const { transformedCategories } = useCategories();
   return (
     <div className="flex items-center justify-between text-secondary-700 mb-8">
       <h1 className="text-lg font-bold">لیست پروژه ها</h1>
-      <div className="">فیلترها...</div>
+      <div className="flex items-center gap-x-8">
+        <Filter filterField="status" options={statusOptions} />
+        <FilterDropDown filterField="sort" options={sortOptions} />
+        <FilterDropDown
+          filterField="category"
+          options={[
+            { value: "ALL", label: "دسته بندی (همه)" },
+            ...transformedCategories,
+          ]}
+        />
+      </div>
     </div>
   );
 }
