3af4f164cf08d15e477a28fa54429ee9140a5e1b
amirreza1374 <126147703+amirreza1374@users.noreply.github.com>
2024-01-11 12:28:47 +0330

    add: useMoveBack to projectHeader

--- src/features/project/ProjectHeader.jsx
+++ src/features/project/ProjectHeader.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { HiArrowRight } from "react-icons/hi";
+import useMoveBack from "../../hooks/useMoveBack";
 
 function ProjectHeader({ project }) {
+  const moveBack = useMoveBack();
   return (
     <div className="flex items-center gap-x-4 mb-8">
-      <button>
+      <button onClick={moveBack}>
         <HiArrowRight className="size-5 text-secondary-500" />
       </button>
       <h1 className="font-black text-secondary-700 text-xl">
