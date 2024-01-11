e00f8b3a2934ffc97b4b2ffd2b0bed723cf7d1f8
amirreza1374 <126147703+amirreza1374@users.noreply.github.com>
2024-01-11 12:28:25 +0330

    change: changeProposalStatus

--- src/features/project/ChangeProposalStatus.jsx
+++ src/features/project/ChangeProposalStatus.jsx
@@ -33,7 +33,7 @@ function ChangeProposalStatus({ proposalId, onClose }) {
 
   const onSubmit = (data) => {
     changeProposalStatus(
-      { id: proposalId, data },
+      { proposalId, projectId, ...data },
       {
         onSuccess: () => {
           onClose();
